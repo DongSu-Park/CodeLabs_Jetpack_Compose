b7cd2654bd6bdc5304f64b9ac6e51bc0c77d7ef9
Park DongSu <parkds0875@gmail.com>
2022-12-21 11:43:16 +0900

    앱에 이미지 추가

--- BasicLayout/app/src/main/java/kr/dev/dspark/textcomposables/MainActivity.kt
+++ BasicLayout/app/src/main/java/kr/dev/dspark/textcomposables/MainActivity.kt
@@ -3,15 +3,18 @@ package kr.dev.dspark.textcomposables
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import kr.dev.dspark.textcomposables.ui.theme.TextComposablesTheme
 
@@ -24,7 +27,10 @@ class MainActivity : ComponentActivity() {
                 Surface(
                     modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background
                 ) {
-                    BirthdayGreetingWithText(msg = "Happy Birthday Sam!", from = "- from Emma")
+                    BirthdayGreetingWithImage(
+                        msg = getString(R.string.happy_birthday_text),
+                        from = getString(R.string.signature_text)
+                    )
                 }
             }
         }
@@ -32,19 +38,42 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun BirthdayGreetingWithText(msg : String, from : String) {
+fun BirthdayGreetingWithText(msg: String, from: String) {
     // Text Composable 만들고 text, fontSize 지정 하여 재사용 처리
     // Column 으로 행으로 처리 (vertical 방향으로)
     Column {
         Text(
             text = msg,
-            fontSize = 36.sp
+            fontSize = 36.sp,
+            modifier = Modifier
+                .fillMaxWidth().wrapContentWidth(Alignment.CenterHorizontally)
+                .padding(start = 16.dp, top = 16.dp)
         )
 
         Text(
             text = from,
-            fontSize = 24.sp
+            fontSize = 24.sp,
+            modifier = Modifier
+                .fillMaxWidth().wrapContentWidth(Alignment.CenterHorizontally)
+                .padding(start = 16.dp, end = 16.dp)
         )
+    }
+}
+
+@Composable
+fun BirthdayGreetingWithImage(msg: String, from: String) {
+    // 이미지 리소스
+    val image = painterResource(id = R.drawable.androidparty)
+
+    // painter -> 표시할 이미지 객체, contentDescription -> 이미지 설명 (talkback 기능)
+    Box {
+        Image(
+            painter = image,
+            contentDescription = null,
+            modifier = Modifier.fillMaxHeight().fillMaxWidth(),
+            contentScale = ContentScale.Crop
+        )
+        BirthdayGreetingWithText(msg = msg, from = from)
     }
 }
 
@@ -54,6 +83,7 @@ fun BirthdayGreetingWithText(msg : String, from : String) {
 fun BirthdayCardPreview() {
     // Composable 함수 이름의 첫 글자는 대문자 (파스칼 표기법)
     TextComposablesTheme() {
-        BirthdayGreetingWithText(msg = "Happy Birthday Sam!", from = "- from Emma")
+//        BirthdayGreetingWithText(msg = "Happy Birthday Sam!", from = "- from Emma")
+        BirthdayGreetingWithImage(msg = "Happy Birthday Sam!", from = "- from Emma")
     }
 }
