3bf5cf9037f4d4607ad91b62a3bb991dd3e4a5e6
Park DongSu <parkds0875@gmail.com>
2022-12-29 11:13:35 +0900

    ArtSpace 스타일 지정

--- ArtSpace/app/src/main/java/kr/dev/dspark/artspace/MainActivity.kt
+++ ArtSpace/app/src/main/java/kr/dev/dspark/artspace/MainActivity.kt
@@ -3,7 +3,9 @@ package kr.dev.dspark.artspace
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
@@ -15,13 +17,16 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import kr.dev.dspark.artspace.model.ArtItem
 import kr.dev.dspark.artspace.ui.theme.ArtSpaceTheme
 
 class MainActivity : ComponentActivity() {
@@ -55,30 +60,46 @@ fun ArtSpaceLayout() {
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
-        Column(modifier = Modifier.padding(20.dp)) {
+        Surface(
+            border = BorderStroke(width = 4.dp, Color.Gray),
+            elevation = 16.dp,
+            modifier = Modifier.padding(20.dp)
+        ) {
             Image(
                 painter = painterResource(id = R.drawable.open_image_first),
                 contentDescription = null,
-                modifier = Modifier.padding(20.dp)
+                modifier = Modifier.padding(20.dp).fillMaxWidth().height(400.dp)
             )
         }
 
         Spacer(modifier = Modifier.height(8.dp))
 
-        Column(
+        Surface(
+            elevation = 8.dp,
             modifier = Modifier
                 .fillMaxWidth()
-                .padding(start = 20.dp, end = 20.dp),
-            horizontalAlignment = Alignment.CenterHorizontally,
+                .padding(start = 20.dp, end = 20.dp)
         ) {
-            Text(
-                text = "Artwork Title",
-                fontSize = 30.sp,
-                textAlign = TextAlign.Center,
-                fontWeight = FontWeight.Bold
-            )
-            Spacer(modifier = Modifier.height(5.dp))
-            Text(text = "Artwork Artist (Year)")
+            Column(modifier = Modifier.padding(20.dp)) {
+                Text(
+                    text = "Artwork Title",
+                    fontSize = 28.sp,
+                    textAlign = TextAlign.Justify,
+                    fontWeight = FontWeight.Light
+                )
+                Spacer(modifier = Modifier.height(5.dp))
+
+                Text(
+                    buildAnnotatedString {
+                        withStyle(style = SpanStyle(fontWeight = FontWeight.Bold, color = Color.Black)) {
+                            append(text = "Artwork Artist" + " ")
+                        }
+                        withStyle(style = SpanStyle(fontWeight = FontWeight.ExtraLight)) {
+                            append(text = "(" + "Year" + ")")
+                        }
+                    }
+                )
+            }
         }
 
         Spacer(modifier = Modifier.height(8.dp))
